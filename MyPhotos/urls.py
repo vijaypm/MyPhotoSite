cfac82f15991d6afc0c6b91ebfa5789550e15075
Vijay Mariadassou <vijaypmreg+github@gmail.com>
2013-09-14 09:28:51 -0700

    added views

---
+++ MyPhotos/urls.py
@@ -0,0 +1,10 @@
+__author__ = 'vijay'
+from django.conf.urls import patterns, url
+
+from MyPhotos import views
+
+urlpatterns = patterns('',
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<user_id>\d+)/albums/$', views.albums, name='albums'),
+    url(r'^(?P<album_id>\d+)/photos/$', views.photos, name='photos'),
+)
--- MyPhotos/views.py
+++ MyPhotos/views.py
@@ -1,1 +1,33 @@
-# Create your views here.
+from django.http import HttpResponse
+# from django.template import RequestContext, loader
+# from django.http import Http404
+from django.shortcuts import render, get_object_or_404
+
+from MyPhotos.models import User
+
+
+def index(request):
+    user_list = User.objects.order_by('username')
+    # Approach 1
+    # output = ', '.join(str(u) for u in user_list) # join([str(u) for u in user_list]) # map(str, user_list)
+    # return HttpResponse(output)
+    # Approach 2
+    # template = loader.get_template('myphotos/index.html')
+    # context = RequestContext(request, {
+    #     'user_list':user_list,
+    # })
+    # return HttpResponse(template._render(context))
+    # Approach 3
+    context = {'user_list':user_list}
+    return render(request, 'myphotos/index.html', context)
+
+def albums(request, user_id):
+    # try:
+    #     user = User.objects.get(pk=user_id)
+    # except User.DoesNotExist:
+    #     raise Http404
+    user = get_object_or_404(User, pk=user_id)
+    return render(request, 'myphotos/albums.html', {'user':user})
+
+def photos(request, album_id):
+    return HttpResponse("You are viewing photos in album %s" % album_id)
