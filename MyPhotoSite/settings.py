8767556af295ffa3f528c3d8dfef3ca654f6aac8
Vijay Mariadassou <vijaypmreg+github@gmail.com>
2013-10-03 08:50:31 -0700

    fixed path of sqlite db to be relative to project folder

--- MyPhotoSite/settings.py
+++ MyPhotoSite/settings.py
@@ -13,7 +13,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/vijay/PycharmProjects/MyPhotoSite/data/sqlite3.db', # Or path to database file if using sqlite3.
+        'NAME': 'data/sqlite3.db', # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
